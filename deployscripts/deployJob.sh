ebf6ee80bbca1ad960ba5a4ba4ff1bc2fa95b1cd
kiran <kiranops@yahoo.com>
2022-02-09 23:29:51 +0530

    updated artifacts path location

--- deployscripts/deployJob.sh
+++ deployscripts/deployJob.sh
@@ -2,19 +2,18 @@
 
 
 RESOURCE_GROUP=$1
-ARTIFACT_PATH=$2
+
 chmod 400 id_rsa
 
-echo $ARTIFACT_PATH
-ls -al $ARTIFACT_PATH
-
-ARTIFACT_NAME=$(realpath $ARTIFACT_PATH/*.war)
-
+ARTIFACT_NAME=$(ls -Art /home/azadmin/agent/artifacts/*.war | tail -n 1)
 echo $ARTIFACT_NAME
+
+ARTIFACT_PATH=$(realpath $ARTIFACT_NAME)
+echo $ARTIFACT_PATH
 
 echo -e "Host 10.*\n \tStrictHostKeyChecking no" > ~/.ssh/config
 
-sed -i "s,\#warfilePath\#,$warfilePath,g" deployscripts/vars.yaml
+sed -i "s,\#warfilePath\#,$ARTIFACT_PATH,g" deployscripts/vars.yaml
 
 cat deployscripts/vars.yaml
 
